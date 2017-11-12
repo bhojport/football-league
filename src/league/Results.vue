69ee06744bacbd33a1d2fcb255127875d3c76d4f
Bhojendra Rauniyar <bhojport@gmail.com>
2017-11-12 22:50:56 +0545

    WIP Code: Fetch results data

--- src/league/Results.vue
+++ src/league/Results.vue
@@ -11,6 +11,17 @@
                 </div>
             </div>
         </dl>
+        <button @click="fetchResults">Fetch Results</button>
+        <dl>
+            <div v-for="(value,key) in resultsData" :key="key">
+                <dt class="mt-3">{{key}}</dt>
+								<div class="row" v-for="(v,i) in value" :key="i">
+									<dd class="col-5" :class="{'text-success': v.sc1 > v.sc2}">{{v.tn1}} ({{v.sc1}})</dd>
+									<dd class="col-2">vs</dd>
+									<dd class="col-5" :class="{'text-success':v.sc2 > v.sc1}">{{v.tn2}} ({{v.sc2}})</dd>
+								</div>
+            </div>
+        </dl>
     </div>
 </template>
 <script>
@@ -33,8 +44,33 @@ export default {
                     {date: '12/10/2017', teamA: 'Everton', teamB: 'Swansea', scoreA: 1, scoreB: 1}
                 ]
             }
-        ]
+        ],
+        resultsData: []
       }
+  },
+  methods: {
+      fetchResults(){
+        this.$http
+						.get('https://football-league-c088e.firebaseio.com/results.json')
+						.then(response => {
+								return response.json();
+						}, error => {
+								console.log(error);
+						})
+						.then(results => {
+								const resultsArr = [];
+								for(let key in results){
+										// console.log(results[key]);
+										resultsArr.push(results[key]);
+								}
+							const resData = resultsArr.reduce(function (r, a) {
+									r[a.date] = r[a.date] || [];
+									r[a.date].push(a);
+									return r;
+							}, Object.create(null));
+							this.resultsData = resData;
+						});
+    }
   }
 }
 </script>
