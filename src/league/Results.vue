bf18e9ff7c508b2b16fc19225bdd834141126bd5
Bhojendra Rauniyar <bhojport@gmail.com>
2017-11-12 11:28:17 +0545

    Static results page setup

--- src/league/Results.vue
+++ src/league/Results.vue
@@ -1,6 +1,35 @@
 <template>
     <div id="results">
-        <h1>Results</h1>
-        <p>Results View</p>
+        <h1 class="text-center">Results</h1>
+        <dl>
+            <dt class="mt-3">12/11/2017</dt>
+            <div class="row">
+                <dd class="col-5">Aston Villa (1)</dd>
+                <dd class="col-2">vs</dd>
+                <dd class="col-5 text-success">Manchester Utd (3)</dd>
+            </div>
+            <div class="row">
+                <dd class="col-5">Liverpool (1)</dd>
+                <dd class="col-2">vs</dd>
+                <dd class="col-5 text-success">Leads (1)</dd>
+            </div>
+
+            <dt class="mt-3">12/10/2017</dt>
+            <div class="row">
+                <dd class="col-5">Liverpool (4)</dd>
+                <dd class="col-2">vs</dd>
+                <dd class="col-5 text-success">Stroke City (7)</dd>
+            </div>
+            <div class="row">
+                <dd class="col-5 text-success">Man City (2)</dd>
+                <dd class="col-2">vs</dd>
+                <dd class="col-5">Watford (1)</dd>
+            </div>
+            <div class="row">
+                <dd class="col-5 text-success">Everton (1)</dd>
+                <dd class="col-2">vs</dd>
+                <dd class="col-5">Swansea (1)</dd>
+            </div>
+        </dl>
     </div>
 </template>
