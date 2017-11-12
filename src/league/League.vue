1a07d9ab377f3b226085f6ef6e8d6928b67a4ab7
Bhojendra Rauniyar <bhojport@gmail.com>
2017-11-12 12:28:15 +0545

    Static league table setup

--- src/league/League.vue
+++ src/league/League.vue
@@ -1,6 +1,83 @@
 <template>
     <div id="league">
-        <h1>League</h1>
-        <p>League View</p>
+        <h1 class="text-center">League Table</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Pos</th>
+                    <th scope="col">Team v t e</th>
+                    <th scope="col">Pld</th>
+                    <th scope="col">W</th>
+                    <th scope="col">D</th>
+                    <th scope="col">L</th>
+                    <th scope="col">Pts</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Chelsea (C)</td>
+                    <td>38</td>
+                    <td>30</td>
+                    <td>3</td>
+                    <td>5</td>
+                    <td>93</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Tottenham Hotspur</td>
+                    <td>38</td>
+                    <td>26</td>
+                    <td>8</td>
+                    <td>4</td>
+                    <td>86</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Manchestar City</td>
+                    <td>38</td>
+                    <td>29</td>
+                    <td>9</td>
+                    <td>6</td>
+                    <td>78</td>
+                </tr>
+                <tr>
+                    <th scope="row">4</th>
+                    <td>Liverpool</td>
+                    <td>38</td>
+                    <td>22</td>
+                    <td>10</td>
+                    <td>6</td>
+                    <td>76</td>
+                </tr>
+                <tr>
+                    <th scope="row">5</th>
+                    <td>Arsenal</td>
+                    <td>38</td>
+                    <td>23</td>
+                    <td>6</td>
+                    <td>9</td>
+                    <td>75</td>
+                </tr>
+                <tr>
+                    <th scope="row">6</th>
+                    <td>Manchestar United</td>
+                    <td>38</td>
+                    <td>18</td>
+                    <td>15</td>
+                    <td>5</td>
+                    <td>69</td>
+                </tr>
+                <tr>
+                    <th scope="row">5</th>
+                    <td>Everton</td>
+                    <td>38</td>
+                    <td>17</td>
+                    <td>10</td>
+                    <td>11</td>
+                    <td>68</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
